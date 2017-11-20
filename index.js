9a8df94517371d05d18bee841325a9e7a8dca184
Coob2 <coob2_17@uni.worc.ac.uk>
2017-11-20 10:50:19 +0000

    Added location listener

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ $(document).on('pageinit', function() {
 
 	//change time box to show message
 	$('#time').val("Press the button to get location data");
-
 });
-
 
 //Call this function when you want to get the current position
 function getPosition() {
@@ -17,18 +15,24 @@ function getPosition() {
 	$('#time').val("Getting data...");
 
 	//instruct location service to get position with appropriate callbacks
-	navigator.geolocation.getCurrentPosition(successPosition, failPosition);
+	//navigator.geolocation.getCurrentPosition(successPosition, failPosition);
+	navigator.geolocation.watchPosition(successPosition, failPosition, locationOptions);
+
 }
 
+//set location options
+var locationOptions = {
+maximumAge: 3000,
+timeout: 5000,
+enableHighAccuracy: true};
 
 //called when the position is successfully determined
 function successPosition(position) {
 
 	//You can find out more details about what the position obejct contains here:
 	// http://www.w3schools.com/html/html5_geolocation.asp
-
-
 	//lets get some stuff out of the position object
+
 	var time = position.timestamp;
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
@@ -37,12 +41,10 @@ function successPosition(position) {
 	$('#time').val("Recieved data at " + time);
 	$('#lattext').val("Latitude is " + latitude);
 	$('#longtext').val("Longitude is " + longitude);
-
 }
 
 //called if the position is not obtained correctly
 function failPosition(error) {
 	//change time box to show updated message
 	$('#time').val("Error getting data: " + error);
-
 }
